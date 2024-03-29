0043670e3b394d46da10b7a0e5ae65c0641e2077
Liza Ponomarenko <popelvis.ragul@gmail.com>
2024-03-29 21:59:49 +0100

    database is displayed at

--- testtask/main/views.py
+++ testtask/main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Projects
+from django.core.paginator import Paginator
 
 
 def show_projects(request):
-    return render(request, 'main/projects.html')
+    data = Projects.objects.all()
+    paginator = Paginator(data, 100)  
+    page_number = request.GET.get('page')  
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'main/projects.html', {'page_obj': page_obj})
