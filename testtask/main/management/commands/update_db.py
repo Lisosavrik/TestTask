9b6823c5d281d94e5024456c8e358a178d9fa448
Liza Ponomarenko <popelvis.ragul@gmail.com>
2024-03-30 17:52:25 +0100

    fix bug with update_db commard

--- testtask/main/management/commands/update_db.py
+++ testtask/main/management/commands/update_db.py
@@ -10,6 +10,10 @@ class Command(BaseCommand):
         url= f"https://search.worldbank.org/api/v2/projects?format=json&rows=200&fct=projectfinancialtype_exact,status_exact,regionname_exact,themev2_level1_exact,themev2_level2_exact,themev2_level3_exact,sector_exact,countryshortname_exact,cons_serv_reqd_ind_exact,esrc_ovrl_risk_rate_exact&fl=id,regionname,countryname,projectstatusdisplay,project_name,countryshortname,pdo,impagency,cons_serv_reqd_ind,url,boardapprovaldate,closingdate,projectfinancialtype,curr_project_cost,ibrdcommamt,idacommamt,totalamt,grantamt,borrower,lendinginstr,envassesmentcategorycode,esrc_ovrl_risk_rate,sector1,sector2,sector3,theme1,theme2,%20%20status,totalcommamt,proj_last_upd_date,curr_total_commitment,curr_ibrd_commitment,curr_ida_commitment,last_stage_reached_name,theme_list,ida_cmt_usd_amt,cmt_usd_amt,projectcost&apilang=en&os=0"
         response = requests.get(url)
         obj = response.json()
+
+        all_data = Projects.objects.all()
+        all_data.delete()
+
         for project in obj['projects']:
             project_dict = obj['projects'][project]
 
